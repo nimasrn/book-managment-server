56b3213dd98abcdf91bf769a723e1ef1522cacdb
nimasrn <nimasrn.com@gmail.com>
2020-10-04 19:47:40 +0330

    add some global middlwares (cors helmet morgan validator)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cors } from 'cors';
+// import morgan from 'morgan';
+import * as morgan from 'morgan';
+import * as helmet from 'helmet';
+import { logger } from './middleware/logger.middleware';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logger);
+  app.use(helmet());
+  app.enableCors();
+  app.use(morgan('dev'));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
