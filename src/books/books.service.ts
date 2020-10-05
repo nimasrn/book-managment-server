fdafc39b74b6eef1235231a5eb762b7efda1503a
nimasrn <nimasrn.com@gmail.com>
2020-10-05 18:06:35 +0330

    add crud function

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,15 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { Book } from './interfaces/book.interface';
+import { HttpService, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Observable } from 'rxjs';
+import { Repository } from 'typeorm';
+import { Book } from './book.entity';
+import { CreateBookDto } from './dto/create.dto';
+import { UpdateBookDto } from './dto/update.dto';
 
 @Injectable()
 export class BooksService {
-  private readonly books: Book[] = [];
-
-  create(book: Book) {
-    return this.books.push(book);
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>,
+    private httpService: HttpService
+  ) { }
+  create({ title, ISBN }): Promise<Book> {
+    const book = new Book();
+    book.title = title;
+    book.ISBN = ISBN;
+    return this.bookRepository.save(book);
   }
 
-  findAll(): Book[] {
-    return this.books;
+  findOneByISBN(ISBN): Promise<Book> {
+    return this.bookRepository.findOne({ ISBN: ISBN });
+  }
+
+  findAll(): Promise<Book[]> {
+    return this.bookRepository.find();
+  }
+
+  getDetailsByISBN(ISBN: string): Observable<any> {
+    return this.httpService.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${ISBN}&jscmd=data&format=json`);
+  }
+
+  // getAuthor(authorUrl: string): Observable<any> {
+  //   return this.httpService.get(`https://openlibrary.org/${authorUrl}.json`);
+  // }
+
+  updatebyId(id: any, book: UpdateBookDto): Promise<any> {
+    return this.bookRepository.update(id, book);
+  }
+
+  delete(id: number): Promise<any> {
+    return this.bookRepository.delete(id);
   }
 }
