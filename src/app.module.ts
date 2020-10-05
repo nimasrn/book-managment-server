b1f0a3f444bf3790b752cc1f5e48a8a004de228b
nimasrn <nimasrn.com@gmail.com>
2020-10-05 12:32:43 +0330

    add user packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BookModule } from './books/book.module';
 
-import { BooksController } from './books/books.controller';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Connection } from 'typeorm';
 @Module({
-  imports: [BookModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    BookModule,
+    AuthModule,
+    UsersModule
+  ],
+  controllers: [
+    // AppController
+  ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
