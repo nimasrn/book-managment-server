9e18cbc0ac3db89e4799b70e6e9981323dc4b4a6
nimasrn <nimasrn.com@gmail.com>
2020-10-05 18:07:03 +0330

    add HttpModule

--- src/books/book.module.ts
+++ src/books/book.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 
 @Module({
-  imports: [],
-  controllers: [
-    BooksController
-  ],
+  imports: [TypeOrmModule.forFeature([Book]), HttpModule],
+  controllers: [BooksController],
   providers: [BooksService],
   exports: [BooksService]
 })
