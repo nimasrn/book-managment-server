e3af6c07606c22e80c4c8425b28f24dd6abce686
nimasrn <nimasrn.com@gmail.com>
2020-10-05 18:06:13 +0330

    crud in book controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,18 +1,50 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Req, UsePipes } from '@nestjs/common';
+import { string } from '@hapi/joi';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, Req, UnprocessableEntityException, UseGuards, UsePipes } from '@nestjs/common';
+import { stringify } from 'querystring';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { Book } from './book.entity';
 import { BooksService } from './books.service';
-import { CreateBookDto } from './dto/dto';
-import { Book } from './interfaces/book.interface';
+import { CreateBookDto } from './dto/create.dto';
+import { UpdateBookDto } from './dto/update.dto';
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) { }
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    this.booksService.create(createBookDto);
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() createBookDto: CreateBookDto) {
+    const book = await this.booksService.findOneByISBN(createBookDto.ISBN);
+    if (book) {
+      throw new HttpException('The book with ISBN already exist', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    const createdBook = await this.booksService.create(createBookDto);
+
+    this.booksService.getDetailsByISBN(createBookDto.ISBN).subscribe(async res => {
+      const { authors, cover } = res.data[`ISBN:${createBookDto.ISBN}`];
+      createdBook.authors = authors;
+      createdBook.cover = cover;
+      await this.booksService.updatebyId(createdBook.id, createdBook)
+    })
+
+    return createdBook;
   }
 
   @Get()
   async findAll(): Promise<Book[]> {
-    return this.booksService.findAll();
+    const books = await this.booksService.findAll();
+    return books;
+  }
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  async edit(@Param() params, @Body() updateBookDto: UpdateBookDto): Promise<Book[]> {
+    const book = await this.booksService.updatebyId(params.id, updateBookDto);
+    return book;
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  async delete(@Param() params): Promise<Book> {
+    return this.booksService.delete(params.id);
   }
 }
