f2aae28173a4beaba3effd731239cd656d638a84
nimasrn <nimasrn.com@gmail.com>
2020-10-05 12:33:43 +0330

    auth controler

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+
+import { Controller, Get, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
