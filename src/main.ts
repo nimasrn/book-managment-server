72074173810ef8e503edfbcbc87225400fa49d39
nimasrn <nimasrn.com@gmail.com>
2020-10-06 20:23:13 +0330

    add process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
       "Too many requests from this IP, please try again later"
   }));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
